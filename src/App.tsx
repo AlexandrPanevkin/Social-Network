6d847bedf88a2d86bfb8e6ecf05f77a7f275ef4c
AlexanderPanevkin <panevkin900@gmail.com>
2022-10-12 13:00:54 +0300

    add samurai, grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import samuraiSvg from "./assets/img/samurai.svg";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img className="header__logo-icon" src={samuraiSvg} alt="Samurai"/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className="content">
+            </div>
         </div>);
 }
-
 export default App;
