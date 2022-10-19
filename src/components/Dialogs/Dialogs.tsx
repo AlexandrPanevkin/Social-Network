9d9760b7cdf12d88d0f40a4a4f36fe2bbc59ab45
AlexanderPanevkin <panevkin900@gmail.com>
2022-10-19 13:57:57 +0300

    add Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,43 @@
 import React from "react";
+import './Dialogs.css';
+import {NavLink} from "react-router-dom";
+
+type DialogItemType = {
+    id: number
+    name: string
+}
+const DialogItem = (props: DialogItemType) => {
+    return (
+        <div className="item"><NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink></div>
+    )
+}
+
+type MessageType = {
+    message: string
+}
+
+const Message = (props: MessageType) => {
+    return (
+    <div className="message">{props.message}</div>
+    )
+}
 
 
 export const Dialogs = () => {
     return (
         <div className="dialogs">
-            Dialogs
+            <div className="dialogs-items">
+                <DialogItem id={1} name={'Alex'}/>
+                <DialogItem id={2} name={'Valera'}/>
+                <DialogItem id={3} name={'Sergey'}/>
+                <DialogItem id={4} name={'Nikita'}/>
+                <DialogItem id={5} name={'Ivan'}/>
+            </div>
+            <div className="messages">
+               <Message message={"Hi"}/>
+                <Message message={"Hello"}/>
+                <Message message={"How are you?"}/>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,10 +4,10 @@ import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return   <nav className="navbar">
-        <div className="nav"><NavLink to='/profile'>Profile</NavLink></div>
-        <div className="nav"><NavLink to='/dialogs'>Messages</NavLink></div>
-        <div className="nav"><NavLink to='/news'>News</NavLink></div>
-        <div className="nav"><NavLink to='/music'>Music</NavLink></div>
-        <div className="nav"><NavLink to='/settings'>Settings</NavLink></div>
+        <div className="navbar-nav"><NavLink to='/profile'>Profile</NavLink></div>
+        <div className="navbar-nav"><NavLink to='/dialogs'>Messages</NavLink></div>
+        <div className="navbar-nav"><NavLink to='/news'>News</NavLink></div>
+        <div className="navbar-nav"><NavLink to='/music'>Music</NavLink></div>
+        <div className="navbar-nav"><NavLink to='/settings'>Settings</NavLink></div>
     </nav>
 }
