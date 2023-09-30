3571a6df522215945fdc2e99b297eea8d2b701a8
AlexanderPanevkin <panevkin900@gmail.com>
2023-09-30 18:47:25 +0300

    feat: add test for add post in profileReducer

--- src/Redux/profileReducer.test.ts
+++ src/Redux/profileReducer.test.ts
@@ -1,17 +1,26 @@
 import {addPostAC, postsType, profileReducer, ProfileType} from "./profileReducer";
 
+let state = {
+    posts: [
+        {id: 1, message: 'post1', likesCount: 7},
+        {id: 2, message: 'post2', likesCount: 2},
+        {id: 3, message: 'post3', likesCount: 4},
+    ] as postsType[],
+    newPostText: '',
+    profile: null as ProfileType | null,
+    status: ''
+}
+
 it('new post should be added', () => {
     let action = addPostAC('lalala')
-    let state = {
-        posts: [
-            {id: 1, message: 'post1', likesCount: 7},
-            {id: 2, message: 'post2', likesCount: 2},
-            {id: 3, message: 'post3', likesCount: 4},
-        ] as postsType[],
-        newPostText: '',
-        profile: null as ProfileType | null,
-        status: ''
-    }
+
     let newState = profileReducer(state, action)
     expect(newState.posts.length).toBe(4)
+})
+
+it('message of new post should be correct', () => {
+    let action = addPostAC('lalala')
+
+    let newState = profileReducer(state, action)
+    expect(newState.posts[0].message).toBe('lalala')
 })
